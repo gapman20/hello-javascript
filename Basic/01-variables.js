2d1daddc53658bdf640868218642be9c5ac1e065
Gabriel <jgabriel96@hotmail.com>
2024-07-15 11:38:40 -0600

    Primera clase de JavaScript

---
+++ Basic/01-variables.js
@@ -0,0 +1,24 @@
+//var
+
+var helloWorld  = "¡Hola, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+//let
+
+let helloWorld2 = "¡Hola, JavaScript!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld2)
+
+//const
+
+const helloWorld3 = "¡Hola, JavaScript!"
+console.log(helloWorld3)
+
+//Error
+// helloWorld3 = "¡Hola de nuevo, JavaScript"
+// console.log(helloWorld3)
